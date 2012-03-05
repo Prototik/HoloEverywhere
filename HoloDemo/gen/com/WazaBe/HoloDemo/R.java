c7b8a84c76c725b7220a7d1213aaecdaab72e97c
Christophe Versieux <christophe.versieux@gmail.com>
2012-03-05 19:52:44 +0100

    AlertDialog - WIP

--- HoloDemo/gen/com/WazaBe/HoloDemo/R.java
+++ HoloDemo/gen/com/WazaBe/HoloDemo/R.java
@@ -24,24 +24,36 @@ public final class R {
         public static final int btn_default_normal_holo_light=0x7f02000a;
         public static final int btn_default_pressed_holo_dark=0x7f02000b;
         public static final int btn_default_pressed_holo_light=0x7f02000c;
-        public static final int edit_text_holo_dark=0x7f02000d;
-        public static final int edit_text_holo_light=0x7f02000e;
-        public static final int ic_launcher=0x7f02000f;
-        public static final int textfield_activated_holo_dark=0x7f020010;
-        public static final int textfield_activated_holo_light=0x7f020011;
-        public static final int textfield_bg_activated_holo_dark=0x7f020012;
-        public static final int textfield_bg_default_holo_dark=0x7f020013;
-        public static final int textfield_bg_disabled_focused_holo_dark=0x7f020014;
-        public static final int textfield_bg_disabled_holo_dark=0x7f020015;
-        public static final int textfield_bg_focused_holo_dark=0x7f020016;
-        public static final int textfield_default_holo_dark=0x7f020017;
-        public static final int textfield_default_holo_light=0x7f020018;
-        public static final int textfield_disabled_focused_holo_dark=0x7f020019;
-        public static final int textfield_disabled_focused_holo_light=0x7f02001a;
-        public static final int textfield_disabled_holo_dark=0x7f02001b;
-        public static final int textfield_disabled_holo_light=0x7f02001c;
-        public static final int textfield_focused_holo_dark=0x7f02001d;
-        public static final int textfield_focused_holo_light=0x7f02001e;
+        public static final int dialog_bottom_holo_dark=0x7f02000d;
+        public static final int dialog_bottom_holo_light=0x7f02000e;
+        public static final int dialog_divider_horizontal_holo_dark=0x7f02000f;
+        public static final int dialog_divider_horizontal_holo_light=0x7f020010;
+        public static final int dialog_divider_horizontal_light=0x7f020011;
+        public static final int dialog_full_holo_dark=0x7f020012;
+        public static final int dialog_full_holo_light=0x7f020013;
+        public static final int dialog_middle_holo=0x7f020014;
+        public static final int dialog_middle_holo_dark=0x7f020015;
+        public static final int dialog_middle_holo_light=0x7f020016;
+        public static final int dialog_top_holo_dark=0x7f020017;
+        public static final int dialog_top_holo_light=0x7f020018;
+        public static final int edit_text_holo_dark=0x7f020019;
+        public static final int edit_text_holo_light=0x7f02001a;
+        public static final int ic_launcher=0x7f02001b;
+        public static final int textfield_activated_holo_dark=0x7f02001c;
+        public static final int textfield_activated_holo_light=0x7f02001d;
+        public static final int textfield_bg_activated_holo_dark=0x7f02001e;
+        public static final int textfield_bg_default_holo_dark=0x7f02001f;
+        public static final int textfield_bg_disabled_focused_holo_dark=0x7f020020;
+        public static final int textfield_bg_disabled_holo_dark=0x7f020021;
+        public static final int textfield_bg_focused_holo_dark=0x7f020022;
+        public static final int textfield_default_holo_dark=0x7f020023;
+        public static final int textfield_default_holo_light=0x7f020024;
+        public static final int textfield_disabled_focused_holo_dark=0x7f020025;
+        public static final int textfield_disabled_focused_holo_light=0x7f020026;
+        public static final int textfield_disabled_holo_dark=0x7f020027;
+        public static final int textfield_disabled_holo_light=0x7f020028;
+        public static final int textfield_focused_holo_dark=0x7f020029;
+        public static final int textfield_focused_holo_light=0x7f02002a;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -50,15 +62,19 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
         public static final int hello_button=0x7f040003;
+        public static final int hello_dialog=0x7f040007;
         public static final int hello_edittext=0x7f040004;
+        public static final int hello_edittext_roboto=0x7f040006;
         public static final int hello_textview=0x7f040002;
+        public static final int hello_textview_roboto=0x7f040005;
     }
     public static final class style {
-        public static final int ButtonHoloDark=0x7f050001;
-        public static final int ButtonHoloLight=0x7f050004;
-        public static final int EditTextHoloDark=0x7f050002;
-        public static final int EditTextHoloLight=0x7f050005;
+        public static final int ButtonHoloDark=0x7f050002;
+        public static final int ButtonHoloLight=0x7f050005;
+        public static final int EditTextHoloDark=0x7f050003;
+        public static final int EditTextHoloLight=0x7f050006;
         public static final int Theme_HoloEverywhereDark=0x7f050000;
-        public static final int Theme_HoloEverywhereLight=0x7f050003;
+        public static final int Theme_HoloEverywhereLight=0x7f050004;
+        public static final int dialogHolo=0x7f050001;
     }
 }
--- HoloDemo/src/com/WazaBe/HoloDemo/HoloDemoActivity.java
+++ HoloDemo/src/com/WazaBe/HoloDemo/HoloDemoActivity.java
@@ -1,13 +1,36 @@
 package com.WazaBe.HoloDemo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
 
 public class HoloDemoActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+
+	public void showDialog(View v) {
+
+		// AlertDialog.Builder builder = new
+		// AlertDialog.Builder(this,R.style.HoloDialog);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Work in Progress").setMessage(R.string.hello_dialog);
+		// SEE: http://stackoverflow.com/a/9434511/327402
+		// .setPositiveButton(android.R.string.ok, new
+		// DialogInterface.OnClickListener() {
+		// public void onClick(DialogInterface dialog, int id) {
+		//
+		// }
+		// });
+		builder.create().show();
+
+	}
 }
