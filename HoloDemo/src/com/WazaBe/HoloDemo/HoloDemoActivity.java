6fa13e9ff9b85d33e001332ae555cb40438f63e6
Christophe Versieux <christophe.versieux@gmail.com>
2012-03-12 13:56:04 +0100

    Added help for ActionBarSherlock

--- HoloDemo/src/com/WazaBe/HoloDemo/HoloDemoActivity.java
+++ HoloDemo/src/com/WazaBe/HoloDemo/HoloDemoActivity.java
@@ -1,26 +1,32 @@
 package com.WazaBe.HoloDemo;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 import com.WazaBe.HoloEverywhere.HoloAlertDialogBuilder;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class HoloDemoActivity extends FragmentActivity {
+//If you plan to use ActionBarSherlock, you'll have to extend SherlockActivity
+//public class HoloDemoActivity extends SherlockActivity {
+public class HoloDemoActivity extends Activity {
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getSupportActionBar().setTitle(R.string.app_name);
+		
+		//NOTE: For ActionBarSherlock only
+		//getSupportActionBar().setTitle(R.string.app_name);
+		
 		setContentView(R.layout.main);
 
 	}
 
 	public void showDialog(View v) {
 
-		// AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		HoloAlertDialogBuilder builder = new HoloAlertDialogBuilder(this);
 
 		builder.setTitle("Work in Progress").setMessage(R.string.hello_dialog);
@@ -28,7 +34,7 @@ public class HoloDemoActivity extends FragmentActivity {
 		// BUTTONS ARE NOT SO EASY TO THEME
 		// SEE: http://stackoverflow.com/a/9434511/327402
 
-		Boolean buttonDebug = true;
+		Boolean buttonDebug = false;
 		if (buttonDebug) {
 			builder.setPositiveButton("Positive",
 					new DialogInterface.OnClickListener() {
