10ce96d034ed7076be534217e4a48c71a716a6f8
Christophe <christophe.versieux@gmail.com>
2012-03-15 13:30:56 +0100

    Added CheckBox

---
+++ HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/CheckBoxHolo.java
@@ -0,0 +1,15 @@
+package com.WazaBe.HoloEverywhere;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+
+public class CheckBoxHolo extends CheckBox {
+
+    public CheckBoxHolo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Typeface font = Typeface.createFromAsset(context.getAssets(), "Roboto-Regular.ttf");
+        setTypeface(font);
+    }
+}
