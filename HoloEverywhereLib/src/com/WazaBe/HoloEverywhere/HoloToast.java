1c606835b99548ae830b78c0377635076e6a4384
Pietro Caselani <pc1992@gmail.com>
2012-07-05 20:07:20 -0300

    Add HoloToast

---
+++ HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/HoloToast.java
@@ -0,0 +1,52 @@
+package com.WazaBe.HoloEverywhere;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HoloToast extends Toast {
+
+	public static HoloToast makeText(Context context, CharSequence s,
+			int duration) {
+		HoloToast toast = new HoloToast(context);
+		toast.setDuration(duration);
+		TextView view = new TextView(context);
+		view.setText(s);
+		view.setTextColor(0xFFDADADA);
+		toast.setView(view);
+		return toast;
+	}
+
+	public static HoloToast makeText(Context context, int resId, int duration) {
+		return HoloToast.makeText(context,
+				context.getResources().getString(resId), duration);
+	}
+
+	private View view;
+
+	public HoloToast(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void setText(CharSequence s) {
+		if (view == null) {
+			return;
+		}
+		try {
+			((TextView) view).setText(s);
+		} catch (ClassCastException e) {
+			Log.e("HoloToast",
+					"This HoloToast was not created with HoloToast.makeText", e);
+		}
+	}
+
+	@Override
+	public void setView(View view) {
+		(this.view = view).setBackgroundResource(R.drawable.toast_frame);
+		super.setView(view);
+	}
+
+}
