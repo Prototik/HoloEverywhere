bc8488cf4c46bb993add3436a044224e922e2a60
Sergey <prototypegamez@gmail.com>
2012-08-29 16:24:01 +0800

    DialogHolo and HoloToast renamed to Dialog and Toast, and RadioButtonHolo also deprecated

--- HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/HoloToast.java
+++ HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/HoloToast.java
@@ -1,13 +1,25 @@
 package com.WazaBe.HoloEverywhere;
 
 import android.content.Context;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+@Deprecated
+/*
+ * Use com.WazaBe.HoloEverywhere.Toast
+ */
 public class HoloToast extends Toast {
+	@Deprecated
+	/*
+	 * Use com.WazaBe.HoloEverywhere.Toast
+	 */
+	public HoloToast(Context context) {
+		super(context);
+	}
 
+	@Deprecated
+	/*
+	 * Use com.WazaBe.HoloEverywhere.Toast.makeText(Context, CharSequence, int)
+	 */
 	public static HoloToast makeText(Context context, CharSequence s,
 			int duration) {
 		HoloToast toast = new HoloToast(context);
@@ -19,34 +31,13 @@ public class HoloToast extends Toast {
 		return toast;
 	}
 
+	@Deprecated
+	/*
+	 * Use com.WazaBe.HoloEverywhere.Toast.makeText(Context, int, int)
+	 */
 	public static HoloToast makeText(Context context, int resId, int duration) {
 		return HoloToast.makeText(context,
 				context.getResources().getString(resId), duration);
-	}
-
-	private View view;
-
-	public HoloToast(Context context) {
-		super(context);
-	}
-
-	@Override
-	public void setText(CharSequence s) {
-		if (view == null) {
-			return;
-		}
-		try {
-			((TextView) view).setText(s);
-		} catch (ClassCastException e) {
-			Log.e("HoloToast",
-					"This HoloToast was not created with HoloToast.makeText", e);
-		}
-	}
-
-	@Override
-	public void setView(View view) {
-		(this.view = view).setBackgroundResource(R.drawable.toast_frame);
-		super.setView(view);
 	}
 
 }
--- HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/RadioButtonHolo.java
+++ HoloEverywhereLib/src/com/WazaBe/HoloEverywhere/RadioButtonHolo.java
@@ -7,27 +7,42 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.RadioButton;
 
+@Deprecated
+/*
+ * You should use *Activity instead *AnyView*Holo
+ */
 public class RadioButtonHolo extends RadioButton {
-
+	@Deprecated
+	/*
+	 * You should use *Activity instead *AnyView*Holo
+	 */
 	public RadioButtonHolo(Context context) {
 		super(context);
 	}
 
+	@Deprecated
+	/*
+	 * You should use *Activity instead *AnyView*Holo
+	 */
 	public RadioButtonHolo(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
+	@Deprecated
+	/*
+	 * You should use *Activity instead *AnyView*Holo
+	 */
 	public RadioButtonHolo(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
-	
+
 	@Override
 	@TargetApi(3)
 	protected void onFinishInflate() {
 		super.onFinishInflate();
-		if(!isInEditMode()) {
+		if (!isInEditMode()) {
 			FontLoader.loadFont(this, HoloFont.ROBOTO_REGULAR);
 		}
 	}
-	
+
 }
