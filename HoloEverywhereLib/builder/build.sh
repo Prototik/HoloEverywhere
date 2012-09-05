abecc4afa17690221c8241e485cfdf479b3b502d
Sergey <prototypegamez@gmail.com>
2012-09-05 12:36:02 +0800

    Added missing styles for support Preference Framework on Android >= 4.0

--- HoloEverywhereLib/builder/build.sh
+++ HoloEverywhereLib/builder/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 java -jar builder.jar source.xml ../res/values/styles.xml
+java -jar builder.jar source-new.xml ../res/values-v14/styles.xml
--- HoloEverywhereLib/builder/src/com/WazaBe/HoloEverywhere/Builder/Processer.java
+++ HoloEverywhereLib/builder/src/com/WazaBe/HoloEverywhere/Builder/Processer.java
@@ -1,5 +1,11 @@
 package com.WazaBe.HoloEverywhere.Builder;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -8,6 +14,9 @@ import java.util.regex.Pattern;
 public class Processer {
 	private static final Pattern DEFINE = Pattern.compile(
 			"<define\\sname=\"([a-zA-Z]+)\">(.*?)</define>", Pattern.MULTILINE
+					| Pattern.DOTALL);
+	private static final Pattern DEFINE_FILE = Pattern.compile(
+			"<define\\sfile=\"([a-zA-Z\\.]+)\"\\s/>", Pattern.MULTILINE
 					| Pattern.DOTALL);
 	private static final Pattern INCLUDE = Pattern
 			.compile("<include\\sname=\"([a-zA-Z]+)\"\\s/>");
@@ -19,11 +28,37 @@ public class Processer {
 
 	public static String process(String xml) {
 		Processer processer = new Processer(xml);
-		Matcher matcher = DEFINE.matcher(xml);
+		Matcher matcher = DEFINE_FILE.matcher(xml);
+		while (matcher.find()) {
+			Matcher matcher2 = DEFINE.matcher(readFile(matcher.group(1)));
+			while (matcher2.find()) {
+				processer.defineBlock(matcher2.group(1), matcher2.group(2));
+			}
+		}
+		matcher = DEFINE.matcher(xml);
 		while (matcher.find()) {
 			processer.defineBlock(matcher.group(1), matcher.group(2));
 		}
 		return processer.toString();
+	}
+
+	private static String readFile(String fileName) {
+		try {
+			File file = new File(fileName);
+			InputStream is = new FileInputStream(file);
+			Reader reader = new BufferedReader(new InputStreamReader(is,
+					"utf-8"), 8192);
+			char[] buffer = new char[8192];
+			int read;
+			StringBuilder builder = new StringBuilder();
+			while ((read = reader.read(buffer)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			reader.close();
+			return builder.toString();
+		} catch (Exception e) {
+			return "";
+		}
 	}
 
 	private final Map<String, String> blockMap = new HashMap<String, String>();
@@ -53,6 +88,7 @@ public class Processer {
 	private synchronized void rebuild() {
 		data = new String(xml);
 		data = DEFINE.matcher(data).replaceAll("");
+		data = DEFINE_FILE.matcher(data).replaceAll("");
 		for (int i = 0; i < 3; i++) {
 			Matcher matcher = INCLUDE.matcher(data);
 			while (matcher.find()) {
