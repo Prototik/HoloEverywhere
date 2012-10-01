aae1c284ca1c09d20021c999afcf09f62052816d
Sergey <prototypegamez@gmail.com>
2012-10-01 18:13:27 +0800

    NumberPickerPreference

--- library/src/com/WazaBe/HoloEverywhere/preference/NumberPickerPreference.java
+++ library/src/com/WazaBe/HoloEverywhere/preference/NumberPickerPreference.java
@@ -6,15 +6,13 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import com.WazaBe.HoloEverywhere.R;
 import com.WazaBe.HoloEverywhere.widget.NumberPicker;
-import com.WazaBe.HoloEverywhere.widget.NumberPicker.Formatter;
+import com.WazaBe.HoloEverywhere.widget.NumberPicker.OnScrollListener;
+import com.WazaBe.HoloEverywhere.widget.NumberPicker.OnValueChangeListener;
 
 public class NumberPickerPreference extends DialogPreference {
-<<<<<<< HEAD
 	public static interface OnBindPickerListener {
 		public void onBindPicker(NumberPicker picker,
 				NumberPickerPreference preference);
@@ -52,63 +50,18 @@ public class NumberPickerPreference extends DialogPreference {
 	}
 
 	private NumberPicker lastNumberPicker;
+	private CharSequence message;
 	private int min, max, value;
 	private OnBindPickerListener onBindPickerListener;
 	private long onLongPressUpdateInterval = 0;
 	private OnScrollListener onScrollListener;
 	private OnValueChangeListener onValueChangedListener;
-	private boolean wrapSelectorWhell;
-=======
-	private int mValue = 0;
-	private NumberPicker mNumberPicker;
-
-	/**
-	 * TODO see NumberPickerPreference(Context context, AttributeSet attrs)
-	 * this constructor is broken
-	public NumberPickerPreference(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-	*/
-
-	/**
-	 * TODO there is no button bar using this constructor. This is a workaround for now.
-	 * When fixed we should call this method inside the constructor like EditTextPreference:
-	 * this(context, attrs, R.attr.numberPickerPreferenceStyle);
-	 * and move all the logic into NumberPickerPreference(Context context, AttributeSet attrs, int defStyle)
-	 */ 
-	public NumberPickerPreference(Context context, AttributeSet attrs, int defStyle) {
-		super(context,attrs);
-		mNumberPicker = new NumberPicker(context, attrs);
-		mNumberPicker.setId(R.id.numberPicker);
-		mNumberPicker.setFocusable(true);
-		mNumberPicker.setFocusableInTouchMode(true);
-		mNumberPicker.setEnabled(true);
-
-		// some sane defaults if not provided in xml
-		onSetInitialValue(true, mValue);
-		int mMinValue = 0, mMaxValue = 100;
-		boolean mWrapSelectorWheel = true;
-		TypedArray numberPickerAttrs = context.obtainStyledAttributes(attrs, R.styleable.NumberPickerPreference);
-		mMinValue = numberPickerAttrs.getInt(R.styleable.NumberPickerPreference_minValue, mMinValue);
-		mMaxValue = numberPickerAttrs.getInt(R.styleable.NumberPickerPreference_maxValue, mMaxValue);
-		mWrapSelectorWheel = numberPickerAttrs.getBoolean(R.styleable.NumberPickerPreference_wrapSelectorWheel, mWrapSelectorWheel);
-
-		mNumberPicker.setMinValue(mMinValue);
-		mNumberPicker.setMaxValue(mMaxValue);
-		mNumberPicker.setWrapSelectorWheel(mWrapSelectorWheel);
-
-		/**
-		 * Force dialog resource until constructor issue is fixed.
-		 */
-		setDialogLayoutResource(R.layout.preference_dialog_numberpicker);
-	}
->>>>>>> saik0/npp_fixup
+	private boolean wrapSelectorWheel;
 
 	public NumberPickerPreference(Context context, AttributeSet attrs) {
 		this(context, attrs, R.attr.numberPickerPreferenceStyle);
 	}
 
-<<<<<<< HEAD
 	public NumberPickerPreference(Context context, AttributeSet attrs,
 			int defStyle) {
 		super(context, attrs, defStyle);
@@ -117,39 +70,27 @@ public class NumberPickerPreference extends DialogPreference {
 				R.style.Holo_PreferenceDialog_NumberPickerPreference);
 		min = a.getInteger(R.styleable.NumberPickerPreference_min, 0);
 		max = a.getInteger(R.styleable.NumberPickerPreference_max, 10);
-		wrapSelectorWhell = a.getBoolean(
-				R.styleable.NumberPickerPreference_wrapSelectorWhell, true);
+		wrapSelectorWheel = a.getBoolean(
+				R.styleable.NumberPickerPreference_wrapSelectorWheel, true);
 		a.recycle();
 	}
 
 	protected NumberPicker getLastNumberPicker() {
 		return lastNumberPicker;
-=======
-
-	public NumberPickerPreference(Context context) {
-		this(context, null);
->>>>>>> saik0/npp_fixup
 	}
 
-	/**
-	 * Returns the {@link NumberPicker} widget that will be shown in the dialog.
-	 * 
-	 * @return The {@link NumberPicker} widget that will be shown in the dialog.
-	 */
-	public NumberPicker getNumberPicker() {
-		return mNumberPicker;
+	public int getMax() {
+		return max;
 	}
 
-	/**
-	 * Gets the value from the {@link Formatter}.
-	 * 
-	 * @return The current preference value.
-	 */
-	public int getValue() {
-		return mValue;
+	public CharSequence getMessage() {
+		return message;
 	}
 
-<<<<<<< HEAD
+	public int getMin() {
+		return min;
+	}
+
 	public OnBindPickerListener getOnBindPickerListener() {
 		return onBindPickerListener;
 	}
@@ -157,45 +98,37 @@ public class NumberPickerPreference extends DialogPreference {
 	public long getOnLongPressUpdateInterval() {
 		return onLongPressUpdateInterval;
 	}
-=======
-	/**
-	 * Saves the value to the {@link SharedPreferences}.
-	 * 
-	 * @param value The value to save
-	 */
-	public void setValue(int value) {
-		final boolean wasBlocking = shouldDisableDependents();
->>>>>>> saik0/npp_fixup
 
-		mValue = value;
+	public OnScrollListener getOnScrollListener() {
+		return onScrollListener;
+	}
 
-		persistInt(value);
-
-		final boolean isBlocking = shouldDisableDependents(); 
-		if (isBlocking != wasBlocking) {
-			notifyDependencyChange(isBlocking);
-		}
+	public OnValueChangeListener getOnValueChangedListener() {
+		return onValueChangedListener;
 	}
 
 	public int getValue() {
 		return value;
 	}
 
-	public boolean isWrapSelectorWhell() {
-		return wrapSelectorWhell;
+	public boolean isWrapSelectorWheel() {
+		return wrapSelectorWheel;
 	}
 
 	@Override
 	protected void onBindDialogView(View view) {
 		super.onBindDialogView(view);
-		NumberPicker numberPicker = mNumberPicker;
-		numberPicker.setValue(getValue());
+		lastNumberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
+		if (lastNumberPicker != null) {
+			onBindPickerView(lastNumberPicker);
+		}
+	}
 
-<<<<<<< HEAD
 	protected void onBindPickerView(NumberPicker picker) {
 		picker.setMinValue(min);
 		picker.setMaxValue(max);
 		picker.setValue(value);
+		picker.setWrapSelectorWheel(wrapSelectorWheel);
 		picker.setOnScrollListener(onScrollListener);
 		picker.setOnValueChangedListener(onValueChangedListener);
 		if (onLongPressUpdateInterval > 0) {
@@ -204,37 +137,33 @@ public class NumberPickerPreference extends DialogPreference {
 		if (onBindPickerListener != null) {
 			onBindPickerListener.onBindPicker(picker, this);
 		}
-=======
-		ViewParent oldParent = numberPicker.getParent();
-        if (oldParent != view) {
-    		if (oldParent != null) {
-    			((ViewGroup) oldParent).removeView(numberPicker);
-    		}
-    		((ViewGroup) view).addView(numberPicker, ViewGroup.LayoutParams.WRAP_CONTENT,
-    				ViewGroup.LayoutParams.WRAP_CONTENT);
-        }
->>>>>>> saik0/npp_fixup
 	}
 
 	@Override
 	protected void onDialogClosed(boolean positiveResult) {
 		super.onDialogClosed(positiveResult);
-
+		if (lastNumberPicker == null) {
+			return;
+		}
 		if (positiveResult) {
-			int value = mNumberPicker.getValue();
-			if (callChangeListener(value)) {
-				setValue(value);
+			int newValue = lastNumberPicker.getValue();
+			if (callChangeListener(newValue)) {
+				persistInt(newValue);
+				value = newValue;
 			}
 		}
 	}
 
 	@Override
 	protected Object onGetDefaultValue(TypedArray a, int index) {
-		return a.getInteger(index, 0);
+		try {
+			return Integer.parseInt(a.getString(index));
+		} catch (NumberFormatException e) {
+			return min;
+		}
 	}
 
 	@Override
-<<<<<<< HEAD
 	protected void onPrepareForRemoval() {
 		if (lastNumberPicker != null) {
 			lastNumberPicker.setOnValueChangedListener(null);
@@ -257,62 +186,61 @@ public class NumberPickerPreference extends DialogPreference {
 			lastNumberPicker.setValue(s.value);
 			lastNumberPicker.setWrapSelectorWheel(s.wrapSelectorWhell);
 		}
-=======
-	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-		setValue(restoreValue ? getPersistedInt(mValue) : (Integer) defaultValue);
->>>>>>> saik0/npp_fixup
 	}
 
 	@Override
 	protected Parcelable onSaveInstanceState() {
-<<<<<<< HEAD
 		Parcelable superState = super.onSaveInstanceState();
 		if (isPersistent()) {
 			return superState;
 		}
 		if (lastNumberPicker == null) {
 			return new SavedState(superState, min, max, value,
-					wrapSelectorWhell);
+					wrapSelectorWheel);
 		} else {
 			return new SavedState(superState, lastNumberPicker.getMinValue(),
 					lastNumberPicker.getMaxValue(),
 					lastNumberPicker.getValue(),
 					lastNumberPicker.getWrapSelectorWheel());
-=======
-		final Parcelable superState = super.onSaveInstanceState();
-		if (isPersistent()) {
-			// No need to save instance state since it's persistent
-			return superState;
->>>>>>> saik0/npp_fixup
 		}
-
-		final SavedState myState = new SavedState(superState);
-		myState.value = getValue();
-		return myState;
 	}
 
 	@Override
-	protected void onRestoreInstanceState(Parcelable state) {
-		if (state == null || !state.getClass().equals(SavedState.class)) {
-			// Didn't save state for us in onSaveInstanceState
-			super.onRestoreInstanceState(state);
-			return;
+	protected void onSetInitialValue(boolean restorePersistedValue,
+			Object defaultValue) {
+		if (restorePersistedValue) {
+			value = getPersistedInt(min);
+		} else {
+			value = defaultValue instanceof Integer ? (Integer) defaultValue
+					: Integer.parseInt(defaultValue.toString());
 		}
-
-		SavedState myState = (SavedState) state;
-		super.onRestoreInstanceState(myState.getSuperState());
-		setValue(myState.value);
 	}
 
-	private static class SavedState extends BaseSavedState {
-		int value;
+	@Override
+	public void setDefaultValue(Object defaultValue) {
+		super.setDefaultValue(defaultValue);
+		value = defaultValue instanceof Integer ? (Integer) defaultValue
+				: Integer.parseInt(defaultValue.toString());
+	}
 
-		public SavedState(Parcel source) {
-			super(source);
-			value = source.readInt();
-		}
+	public void setMax(int max) {
+		this.max = max;
+		notifyChanged();
+	}
 
-<<<<<<< HEAD
+	public void setMessage(CharSequence message) {
+		this.message = message;
+	}
+
+	public void setMessage(int resId) {
+		setMessage(getContext().getText(resId));
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+		notifyChanged();
+	}
+
 	public void setOnBindPickerListener(
 			OnBindPickerListener onBindPickerListener) {
 		this.onBindPickerListener = onBindPickerListener;
@@ -322,28 +250,22 @@ public class NumberPickerPreference extends DialogPreference {
 		this.onLongPressUpdateInterval = onLongPressUpdateInterval;
 		notifyChanged();
 	}
-=======
-		@Override
-		public void writeToParcel(Parcel dest, int flags) {
-			super.writeToParcel(dest, flags);
-			dest.writeInt(value);
-		}
->>>>>>> saik0/npp_fixup
 
-		public SavedState(Parcelable superState) {
-			super(superState);
-		}
+	public void setOnScrollListener(OnScrollListener onScrollListener) {
+		this.onScrollListener = onScrollListener;
+		notifyChanged();
+	}
 
-		public static final Parcelable.Creator<SavedState> CREATOR =
-				new Parcelable.Creator<SavedState>() {
-			public SavedState createFromParcel(Parcel in) {
-				return new SavedState(in);
-			}
+	public void setOnValueChangedListener(
+			OnValueChangeListener onValueChangeListener) {
+		onValueChangedListener = onValueChangeListener;
+		notifyChanged();
+	}
 
-			public SavedState[] newArray(int size) {
-				return new SavedState[size];
-			}
-		};
+	public void setRange(int min, int max) {
+		this.min = min;
+		this.max = max;
+		notifyChanged();
 	}
 
 	public void setValue(int value) {
@@ -355,8 +277,8 @@ public class NumberPickerPreference extends DialogPreference {
 		notifyChanged();
 	}
 
-	public void setWrapSelectorWhell(boolean wrapSelectorWhell) {
-		this.wrapSelectorWhell = wrapSelectorWhell;
+	public void setWrapSelectorWheel(boolean wrapSelectorWheel) {
+		this.wrapSelectorWheel = wrapSelectorWheel;
 		notifyChanged();
 	}
 }
