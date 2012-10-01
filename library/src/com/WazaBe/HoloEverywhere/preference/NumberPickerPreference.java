9c24b1ecd81d614f6f4536ea467b5bf7eae4009e
Joel Pedraza <github@saik0.net>
2012-10-01 05:37:58 -0400

    merge Prototik and saik0 NPP impl

--- library/src/com/WazaBe/HoloEverywhere/preference/NumberPickerPreference.java
+++ library/src/com/WazaBe/HoloEverywhere/preference/NumberPickerPreference.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
