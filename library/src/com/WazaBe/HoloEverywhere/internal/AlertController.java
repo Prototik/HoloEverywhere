3682bf0e4aab2a60f54c9f05416afc135dac1251
Sergey <prototypegamez@gmail.com>
2012-10-12 13:55:33 +0800

    And fix incorrect inflater in AlertControllere (#116)

--- library/src/com/WazaBe/HoloEverywhere/internal/AlertController.java
+++ library/src/com/WazaBe/HoloEverywhere/internal/AlertController.java
@@ -30,7 +30,6 @@ import android.widget.CheckedTextView;
 import android.widget.FrameLayout;
 import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -38,6 +37,7 @@ import android.widget.TextView;
 import com.WazaBe.HoloEverywhere.ArrayAdapter;
 import com.WazaBe.HoloEverywhere.LayoutInflater;
 import com.WazaBe.HoloEverywhere.R;
+import com.WazaBe.HoloEverywhere.widget.LinearLayout;
 import com.WazaBe.HoloEverywhere.widget.ListView;
 
 public class AlertController {
@@ -445,7 +445,8 @@ public class AlertController {
 			mWindow.setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
 					WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
 		}
-		mWindow.setContentView(mAlertDialogLayout);
+		mWindow.setContentView(LayoutInflater.inflate(mWindow.getContext(),
+				mAlertDialogLayout));
 		setupView();
 	}
 
