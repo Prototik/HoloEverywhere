f954a3add5e0769cdbd5d4d8f0dde7c9a8c13eba
Prototik <prototypegamez@gmail.com>
2012-11-04 19:55:20 +0800

    Remove Test class from Builder

--- builder/src/org/holoeverywhere/builder/Test.java
+++
@@ -1,9 +0,0 @@
-
-package org.holoeverywhere.builder;
-
-public class Test {
-    public static void main(String[] args) {
-        Processer.process(Parser
-                .parse("/home/prok/workspace/HoloEverywhere Builder/resources/styles.json"));
-    }
-}
