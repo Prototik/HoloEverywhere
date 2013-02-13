fee67b7ffe3333de984ab66f9e6075ed89f8f3f6
Prototik <prototypegamez@gmail.com>
2013-02-13 15:25:29 +0800

    Update #299

--- Bug299/src/org/holoeverywhere/bug/i299/MainActivity.java
+++ Bug299/src/org/holoeverywhere/bug/i299/MainActivity.java
@@ -3,14 +3,9 @@ package org.holoeverywhere.bug.i299;
 
 import java.util.List;
 
-import org.holoeverywhere.ThemeManager;
 import org.holoeverywhere.preference.PreferenceActivity;
 
 public class MainActivity extends PreferenceActivity {
-    static {
-        ThemeManager.setDefaultTheme(ThemeManager.MIXED);
-    }
-
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.settings_headers, target);
