115e177d0bfe757b9fcd729d1e0f126606c35737
Prototik <prototypegamez@gmail.com>
2013-03-30 16:05:20 +0800

    Bug #364, #354

---
+++ Bug364/src/org/holoeverywhere/bug/i364/MainActivity.java
@@ -0,0 +1,14 @@
+
+package org.holoeverywhere.bug.i364;
+
+import org.holoeverywhere.app.Activity;
+
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
