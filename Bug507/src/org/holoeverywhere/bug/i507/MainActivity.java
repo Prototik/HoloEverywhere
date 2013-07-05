8a1eb231e9007d47433bb02479e74c046ae34638
Sergey Shatunov <prototypegamez@gmail.com>
2013-07-05 13:29:45 +0800

    Correct settings for Bug #507

--- Bug507/src/org/holoeverywhere/bug/i507/MainActivity.java
+++ Bug507/src/org/holoeverywhere/bug/i507/MainActivity.java
@@ -1,3 +1,4 @@
+
 package org.holoeverywhere.bug.i507;
 
 import android.os.Bundle;
@@ -8,19 +9,21 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		new AlertDialog.Builder(this).setView(new EditText(this)).show();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(new EditText(builder.getContext()));
+        builder.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
