6ee21a2cbea306bff71b5ae5de97772e43b3ae0c
Sergey Shatunov <prototypegamez@gmail.com>
2013-11-02 11:53:41 +0800

    Issues project #511

---
+++ issues/src/org/holoeverywhere/issues/i511/MainActivity.java
@@ -0,0 +1,15 @@
+package org.holoeverywhere.issues.i511;
+
+import android.os.Bundle;
+
+import org.holoeverywhere.app.Activity;
+import org.holoeverywhere.issues.R;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.i511_main);
+    }
+}
