ff06ce42ef61166667174affb9a9efc357412592
Sergey Shatunov <prototypegamez@gmail.com>
2013-11-11 15:20:17 +0800

    Some optimizations for date/time pickers

--- library/src/org/holoeverywhere/widget/datetimepicker/date/RotateTextView.java
+++ library/src/org/holoeverywhere/widget/datetimepicker/date/RotateTextView.java
@@ -1,14 +1,15 @@
 package org.holoeverywhere.widget.datetimepicker.date;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.text.Layout;
 import android.util.AttributeSet;
-
-import com.nineoldandroids.view.animation.AnimatorProxy;
+import android.view.Gravity;
 
 import org.holoeverywhere.widget.TextView;
 
 public class RotateTextView extends TextView {
-    private final AnimatorProxy mProxy = AnimatorProxy.NEEDS_PROXY ? AnimatorProxy.wrap(this) : null;
+    private final boolean mTopDown;
 
     public RotateTextView(Context context) {
         this(context, null);
@@ -20,29 +21,37 @@ public class RotateTextView extends TextView {
 
     public RotateTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setRotation(270);
-    }
-
-    @Override
-    public float getRotation() {
-        if (mProxy != null) {
-            return mProxy.getRotation();
+        final int gravity = getGravity();
+        if (Gravity.isVertical(gravity) && (gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM) {
+            setGravity((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
+            mTopDown = false;
         } else {
-            return super.getRotation();
-        }
-    }
-
-    @Override
-    public void setRotation(float rotation) {
-        if (mProxy != null) {
-            mProxy.setRotation(rotation);
-        } else {
-            super.setRotation(rotation);
+            mTopDown = true;
         }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.EXACTLY), heightMeasureSpec);
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        final int count = canvas.save();
+        if (mTopDown) {
+            canvas.translate(getWidth(), 0f);
+            canvas.rotate(90f);
+        } else {
+            canvas.translate(0f, getHeight());
+            canvas.rotate(-90f);
+        }
+        canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
+        getPaint().setColor(getCurrentTextColor());
+        final Layout layout = getLayout();
+        if (layout != null) {
+            layout.draw(canvas);
+        }
+        canvas.restoreToCount(count);
     }
 }
